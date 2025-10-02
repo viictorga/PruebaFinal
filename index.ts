87073b70865a5360868d79ade0d8d994db282e95
viictorga <c1dc4mper@gmail.com>
2025-10-02 13:17:14 +0200

    funciones de get

--- index.ts
+++ index.ts
@@ -1,2 +1,17 @@
-console.log("hola que tal.");
-console.log("que tal estas?")
+import axios from "axios";
+
+const laPromesa = axios.get("https://rickandmortyapi.com/api/character");
+
+laPromesa.then((res)=>{
+
+    console.log(res.data);
+
+}).catch((err) =>{
+
+    console.error("Error con el character ", err.message);
+
+}).finally(()=>{
+
+    console.log("Ya esta enviado lo necesario a la API")
+
+})
