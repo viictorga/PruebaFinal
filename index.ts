54243f9612e5b66989b35ae644b55f7002280eb0
viictorga <c1dc4mper@gmail.com>
2025-10-03 10:03:54 +0200

    encontramos los 5 nombres y los guardamos en un bojeto diferente

--- index.ts
+++ index.ts
@@ -1,17 +1,44 @@
 import axios from "axios";
 
-const laPromesa = axios.get("https://rickandmortyapi.com/api/character");
+const laPromesa1 = axios.get("https://rickandmortyapi.com/api/character/1");
+const laPromesa2 = axios.get("https://rickandmortyapi.com/api/character/2");
+const laPromesa3 = axios.get("https://rickandmortyapi.com/api/character/3");
+const laPromesa4 = axios.get("https://rickandmortyapi.com/api/character/4");
 
-laPromesa.then((res)=>{
+type Resultado = {
+    nombres: String[];
+};
 
-    console.log(res.data);
+const valorInicial : Resultado = {
+    nombres: []
+};
 
+laPromesa1.then((res1)=>{
+
+    //console.log(res.data.name);
+    laPromesa2.then((res2)=>{
+
+    
+        laPromesa3.then((res3)=>{
+
+            laPromesa4.then((res4)=>{
+
+                const miArray : any[] = [res1,res2,res3,res4];
+                const arrayResultado = miArray.reduce((acc: Resultado, n)=>{
+
+                    return{
+                        nombres : [...acc.nombres, n.data.name]
+                    }
+
+                },valorInicial)
+
+                console.log(arrayResultado);
+
+            })
+        })
+    })
 }).catch((err) =>{
 
     console.error("Error con el character ", err.message);
-
-}).finally(()=>{
-
-    console.log("Ya esta enviado lo necesario a la API")
 
 })
